466b688783ed57289d245105096b3ac4759020b7
RobbGar <49005244+RobbGar@users.noreply.github.com>
2020-03-08 17:41:09 +0100

    Revert "Setting up app. Part 12. Now working. Also Google search is ok. Implemented "show frequent words". Now printing most frequent words with percentage. Is this the final commit?. NO, it was not. Refactored names & icons. RobbGar committed something.. reviewed & implemented "locate me" in Google maps. Implemented "Are you sure?" message before exiting app. Tryna implement geo-localization."
    This reverts commit 9aa4c104

--- app/src/main/java/pcad2019/pcadapp/MainActivity.java
+++ app/src/main/java/pcad2019/pcadapp/MainActivity.java
@@ -1,33 +1,21 @@
 package pcad2019.pcadapp;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
-    protected LocationManager locationManager;
-    protected LocationListener locationListener;
-    protected Context context;
-    String provider;
-    protected String latitude, longitude;
-    private String IPstring;
+public class MainActivity extends AppCompatActivity {
 
-    private boolean checkIp() { // controlla se è stato inserito un indirizzo ip
-        if (IPstring == null) {
+    private String IPstring ;
+
+    private boolean checkIp(){ // controlla se è stato inserito un indirizzo ip
+        if ( IPstring == null ){
             Toast.makeText(this, "MISSING IP ADRESS", Toast.LENGTH_SHORT).show();
             return false;
         }
@@ -38,64 +26,37 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-        }
     }
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed(){
         AlertDialog.Builder builder = new AlertDialog.Builder(pcad2019.pcadapp.MainActivity.this);
         builder.setMessage("Are you sure? \nYou gonna be disconnected..")
-                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                .setPositiveButton("YES",new DialogInterface.OnClickListener(){
                     public void onClick(DialogInterface dialog, int id) {
                         pcad2019.pcadapp.MainActivity.super.onBackPressed();
                     }
-                }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-            }
+                }).setNegativeButton("NO",new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int id) { }
         });
         builder.create().show();
     }
 
     @Override
-    protected void onStop() {
+    protected void onStop(){
         super.onStop();
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        latitude = "Latitude: "+location.getLatitude();
-        longitude = "Longitude: "+location.getLongitude();
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        Log.d("Latitude", "disable");
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        Log.d("Latitude", "enable");
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        Log.d("Latitude", "status");
-    }
-
-    public void locateMe(View view) {
-        if(latitude.length()>11&&longitude.length()>12) {
-            String lat = latitude.substring(11);
-            String lon = longitude.substring(12);
-            String url = "https://maps.google.com/?q="+lat+","+lon;
+    public void locateMe (View view) {
+        EditText positionText = (EditText) findViewById(R.id.positionText);
+        if (!positionText.getText().toString().isEmpty()) {
+            String url = "https://www.google.it/maps/place/"+positionText.getText().toString();
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
             this.startActivity(browserIntent);
         }
     }
 
-    public void insertDataSend(View view) {
+    public void insertDataSend(View view) { // effettuta il login e se va a buon fine avvia la loginActivity
         if (!checkIp()) return;
         EditText positionText = (EditText) findViewById(R.id.positionText);
         EditText searchText = (EditText) findViewById(R.id.searchText);
